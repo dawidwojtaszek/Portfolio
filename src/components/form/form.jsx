0c7923290e2b5a72a5de6a7efc30e5d6d6ab0a15
unknown <dawidw22@gmail.com>
2022-04-27 12:48:34 +0200

    fix: form fix

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,23 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
 import FormWrap from "./form-style"
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { navigate } from "gatsby"
 
-const ContactForm = () => (
-  <FormWrap name="contact" method="POST" data-netlify="true" action="/sukces">
-    <FormWrap.Label>Email:</FormWrap.Label>
-    <FormWrap.Input type="email" name="email" />
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
-    <FormWrap.Label>Wiadomość:</FormWrap.Label>
-    <FormWrap.Message name="message" />
+const ContactForm = () => {
+  const [state, setState] = useState({})
 
-    <FormWrap.Btn type="submit">
-      <FormWrap.BtnIcon>
-        <FontAwesomeIcon icon={faPaperPlane} />
-      </FormWrap.BtnIcon>
-      Wyślij
-    </FormWrap.Btn>
-  </FormWrap>
-)
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+  return (
+    <FormWrap
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+      action="/sukces"
+      onSubmit={handleSubmit}
+    >
+      <FormWrap.Label>Email:</FormWrap.Label>
+      <FormWrap.Input type="email" name="email" onChange={handleChange} />
+
+      <FormWrap.Label>Wiadomość:</FormWrap.Label>
+      <FormWrap.Message name="message" onChange={handleChange} />
+      <input type="hidden" name="contact" value="contact" />
+      <FormWrap.Btn type="submit">
+        <FormWrap.BtnIcon>
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </FormWrap.BtnIcon>
+        Wyślij
+      </FormWrap.Btn>
+    </FormWrap>
+  )
+}
 
 export default ContactForm
